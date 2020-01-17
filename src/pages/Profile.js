2b3fc59f4a9c6d6807086cbd9d1ebc702e8f9b88
nubelsondev <nubelsondev@hotmail.com>
2020-01-17 00:13:45 +0000

    fix(Profile): show user Github profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import { View } from 'react-native'
+import { WebView } from 'react-native-webview'
 
-const Profile = () => {
-	return <View />
+const Profile = ({ navigation }) => {
+	const githubUsername = navigation.getParam('github_username')
+
+	return (
+		<WebView
+			source={{ uri: `https://github.com/${githubUsername}` }}
+			style={{ flex: 1 }}
+		/>
+	)
 }
 
 export default Profile
